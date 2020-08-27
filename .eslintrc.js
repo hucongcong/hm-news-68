6d5235a53cdd281fbdece3569337eeec27699004
hucongcong <flycc258@163.com>
2020-08-27 12:01:39 +0800

    登录功能完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // key: 规则的名字   off: 关闭这条规则
-    'space-before-function-paren': ['error', 'never']
+    'space-before-function-paren': ['error', 'never'],
+    'comment-directive': 0
   }
 }
